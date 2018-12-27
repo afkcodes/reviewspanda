4ceffc47be377d6a18131645a93cb297144ce852
Ashish Kumar <ashish.kmr@outlook.com>
2018-12-28 01:50:28 +0530

    Updated Admin Page

--- admin/admin.php
+++ admin/admin.php
@@ -2,26 +2,32 @@
 session_start();
 require("../connect.php");
 // echo("{$_SESSION['u_name']}"."<br />");
-
 	if (isset($_POST['submit'])) {
-            $topic= $_POST['edtopic'];
-            $content = $_POST['content'];
-            $postedby= $_POST['admin'];
-            date_default_timezone_set("Asia/Calcutta");
-            $time = date("H:i:s");
-            
-            $sql = "INSERT INTO  editorial (edtopic,edarticle,updated_by,updatetime) VALUES (?,?,?,?)";
-            $stmt= $pdo->prepare($sql);
-            $x = $stmt->execute([$topic,$content,$postedby,$time]);
+$movname= $_POST['movie'];
+$release_d= $_POST['release'];
+$director= $_POST['director'];
+$starcast= $_POST['starcast'];
 
-            if ($x) {
-            	echo 'Posted';
-            	header( 'Location: ' . $_SERVER['PHP_SELF'] );
-            }
-            else {
-            	echo 'Sorry';
-            	header( 'Location: ' . $_SERVER['PHP_SELF'] );
-            }
+$topic= $_POST['edtopic'];
+$content = $_POST['content'];
+$postedby= $_POST['admin'];
+$image = $_FILES['image'];
+$imgname = $_FILES['image']['name'];
+$temp_dest = $_FILES['image']['tmp_name'];
+$file_dest = 'uploads/'.$imgname;
+move_uploaded_file($temp_dest, $file_dest);
+date_default_timezone_set("Asia/Calcutta");
+$time = date("d-M-y");
+$sql = "INSERT INTO  editorial (movname,release_d,director,starcast,edtopic,file_dest,edarticle,updated_by,updatetime) VALUES (?,?,?,?,?,?,?,?,?)";
+$stmt= $pdo->prepare($sql);
+$x = $stmt->execute([$movname,$release_d,$director,$starcast,$topic,$file_dest,$content,$postedby,$time]);
+if ($x) {
+	echo 'Posted';
+	header( 'Location:admin.php?post_successful');
+}
+else {
+	echo 'Sorry';
+}
 	}
 ?>
 <!DOCTYPE html>
@@ -43,11 +49,11 @@ require("../connect.php");
 		
 		<!--------------------------------NAVIGATION BAR ENDS------------------------------------>
 		<div class="container content">
-			<h3 class="text-center "> <strong> <em>BEYONDPREP ADMIN</em> </strong> </h3>
+			<h3 class="text-center "> <strong> <em>ReviewsPanda Admin</em> </strong> </h3>
 			<div class="row">
 				<div class="col-md-2 admin-menu">
 					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-						<a class="nav-link text-center active " id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Add Editorial</a>
+						<a class="nav-link text-center active " id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Add Review</a>
 						<a class="nav-link text-center" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Add XYZ</a>
 						<a class="nav-link text-center" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Add XYZ</a>
 						<a class="nav-link text-center" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">ADD XYZ</a>
@@ -56,16 +62,36 @@ require("../connect.php");
 				<div class="col-md-10 edit-content ">
 					<div class="tab-content" id="v-pills-tabContent">
 						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-							<form action="admin.php" method="POST" enctype="multipart/form-data">
+							<form action="ext.php" method="POST" enctype="multipart/form-data">
 								<div class="form-group">
-									<label for="exampleFormControlInput1">Editorial Heading</label>
-									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Editorial Heading" name="edtopic" required>
+									<label for="exampleFormControlInput1">Movie Name</label>
+									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Movie Name" name="movie" required>
 								</div>
-								
 								<div class="form-group">
-									<label for="exampleFormControlTextarea1">Editorial Content</label>
-									<textarea class="ckeditor form-control " id="editor" row='8' name="content" ></textarea>
+									<label for="exampleFormControlInput1">Release Date</label>
+									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Release Date" name="release" required>
+								</div>
+								<div class="form-group">
+									<label for="exampleFormControlInput1">Director</label>
+									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Director" name="director" required>
+								</div>
+								<div class="form-group">
+									<label for="exampleFormControlTextarea1">Star Cast</label>
+									<textarea class="form-control" row='8' name="starcast"></textarea>
+								</div>
+								<div class="form-group">
+									<label for="exampleFormControlInput1">Review Title</label>
+									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Keep it Cool" name="edtopic" required>
+								</div>
+								<div class="custom-file">
+									<label class="custom-file-label" for="customFile">Choose Poster</label>
+									<input type="file" class="custom-file-input" id="customFile" name="image" >
+									
+								</div>
 
+								<div class="form-group">
+									<label for="exampleFormControlTextarea1">Review</label>
+									<textarea class="ckeditor form-control " id="editor" row='8' name="content" ></textarea>
 									<script>
 									ClassicEditor
 										.create( document.querySelector( '#editor' ) )
@@ -79,7 +105,7 @@ require("../connect.php");
 									</script>
 								</div>
 								<div class="form-group">
-									<label for="exampleFormControlInput1">Added By</label>
+									<label for="exampleFormControlInput1">Reviewed By</label>
 									<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Added by" name="admin" required>
 								</div>
 								<div class="text-center">
@@ -87,8 +113,6 @@ require("../connect.php");
 								</div>
 							</form>
 						</div>
-
-
 						<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Dom </div>
 						<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
 						<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
@@ -97,20 +121,13 @@ require("../connect.php");
 				
 			</div>
 		</div>
-
-
-
 		<!-----------------------------------------FOOTER ---------------------------------------->
 		<footer >
-				<div class=" container text-center"> 
+			<div class=" container text-center">
 				<p> Created with Curiosity |&nbsp;&copy; All Rights Reserved</p>
-				</div>
-
+			</div>
 		</footer>
 		<!------------------------------------Bootsrap JS--------------------------------------->
-
-
-
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
